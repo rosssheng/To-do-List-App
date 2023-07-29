ad296e5851c6481fed5401fdf05f4c9efe4fac09
rosssheng <ross62@bu.edu>
2023-07-29 15:18:06 -0400

    update

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import os
 
 app = Flask(__name__)
 Bootstrap(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL',"sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 db = SQLAlchemy(app)
