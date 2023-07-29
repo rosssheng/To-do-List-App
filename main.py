11b7ee8f73513d35f951b1f9b0439eab8c19db2d
rosssheng <ross62@bu.edu>
2023-07-29 17:14:58 -0400

    update

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 Bootstrap(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL1", "sqlite:///users.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", "8BYkEfBA6O6donzWlSihBXox7C0sKR6b")
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
